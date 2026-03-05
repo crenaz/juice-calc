e0fa5f55295425b59735365332f684089363e455
crenaz <crenaz@gmail.com>
2026-03-04 21:40:06 -0400

    newed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createSignal } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
+
+const FRUITS = [
+  { id: 'lemon',       label: 'Lemon',        multiplier: 20 },
+  { id: 'lime',        label: 'Lime',         multiplier: 25 },
+  { id: 'orange',      label: 'Orange',       multiplier: 10 },
+  { id: 'grapefruit',  label: 'Grapefruit',   multiplier: 6  },
+  { id: 'clementine',  label: 'Clementine',   multiplier: 20 },
+  { id: 'bloodorange', label: 'Blood Orange', multiplier: 12 },
+  { id: 'rhubarb',     label: 'Rhubarb',      multiplier: 15 },
+  { id: 'celery',      label: 'Celery',       multiplier: 10 },
+  { id: 'ginger',      label: 'Ginger',       multiplier: 16 },
+  { id: 'carrot',      label: 'Carrot',       multiplier: 30 },
+];
+
+function FruitRow({ label, multiplier }) {
+  const [qty, setQty] = createSignal(1);
+  const [result, setResult] = createSignal('');
+
+  const calc = () => setResult(qty() * multiplier);
+
+  return (
+    <>
+      {label}:{' '}
+      <input type="number" value={qty()} onInput={e => setQty(Number(e.target.value))} />
+      <input type="button" value="calc" onClick={calc} />
+      {' '}<span>{result()}</span>
+      <br /><br />
+    </>
+  );
+}
 
 function App() {
   return (
@@ -9,17 +40,8 @@ function App() {
         <p>
           <code>This is the rebuilt Juice-calc</code>
         </p>
-        <form action="/">
-          Lemon: <input type="number" id="lemon" value="1" /><input type="button" value="calc" id="lemonb" /> <span id="lemonp"></span><br /><br />
-          Lime: <input type="number" id="lime" value="1" /><input type="button" value="calc" id="limeb" /> <span id="limep"></span><br /><br />
-          Orange: <input type="number" id="orange" value="1" /><input type="button" value="calc" id="orangeb" /> <span id="orangep"></span><br /><br />
-          Grapefruit: <input type="number" id="grapefruit" value="1" /><input type="button" value="calc" id="grapefruitb" /> <span id="grapefruitp"></span><br /><br />
-          Clementine: <input type="number" id="clementine" value="1" /><input type="button" value="calc" id="clementineb" /> <span id="clementinep"></span><br /><br />
-          Blood Orange: <input type="number" id="bloodorange" value="1" /><input type="button" value="calc" id="bloodorangeb" /> <span id="bloodorangep"></span><br /><br />
-          Rhubarb: <input type="number" id="rhubarb" value="1" /><input type="button" value="calc" id="rhubarbb" /> <span id="rhubarbp"></span><br /><br />
-          Celery: <input type="number" id="celery" value="1" /><input type="button" value="calc" id="celeryb" /> <span id="celeryp"></span><br /><br />
-          Ginger: <input type="number" id="ginger" value="1" /><input type="button" value="calc" id="gingerb" /> <span id="gingerp"></span><br /><br />
-          Carrot: <input type="number" id="carrot" value="1" /><input type="button" value="calc" id="carrotb" /> <span id="carrotp"></span><br /><br />
+        <form onSubmit={e => e.preventDefault()}>
+          {FRUITS.map(f => <FruitRow label={f.label} multiplier={f.multiplier} />)}
         </form>
       </header>
     </div>
